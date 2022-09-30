d0be57de751206081e48ba7824343cc56c44a649
erhan <erhanserkansabriye@gmail.com>
2022-09-30 13:22:58 +0300

    workshoplardan 8 olan eklendi 30.09.2022

--- 09.09.2022/code challenge/index.js
+++ 09.09.2022/code challenge/index.js
@@ -90,20 +90,20 @@
 // console.log(intToRoman(1555));
  
 
-const convertToRoman = (num) => {
-    let roman = '';
-    let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    let romanNum = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-    for (let i = 0; i < decimal.length; i++) {
-        while (decimal[i] <= num) {
-            roman += romanNum[i];
-            num -= decimal[i];
-        }
-    }
-    return roman;
-};
+// const convertToRoman = (num) => {
+//     let roman = '';
+//     let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+//     let romanNum = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+//     for (let i = 0; i < decimal.length; i++) {
+//         while (decimal[i] <= num) {
+//             roman += romanNum[i];
+//             num -= decimal[i];
+//         }
+//     }
+//     return roman;
+// };
 
-console.log(convertToRoman(2748));
+// console.log(convertToRoman(2748));
 
 
 // const roman = (num1) => {
